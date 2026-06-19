33e2f7aa6cad4346bbf64396577a70301a852e3c
Owner <a.squared.analytics@gmail.com>
2026-06-19 00:16:17 -0400

    Initial commit, which contains:
    1) loading of data
    2) standardization of date variable name
    3) cleaning and parsing out of event engagement

---
+++ Journey Prevention Investigation.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(stringr)
+library(purrr)
+library(tidyr)
+
+Daily_Checks <- read.csv("C:/Users/Owner/Downloads/Daily Check-ins.csv")
+
+colnames(Daily_Checks)[colnames(Daily_Checks) == "CHECKINDATE"] <- "EVENTDATE"
+
+Monthly_Assessments <- read.csv("C:/Users/Owner/Downloads/Monthly Assessments.csv")
+
+colnames(Monthly_Assessments)[colnames(Monthly_Assessments) == "ASSESSMENTDATE"] <- "EVENTDATE"
+
+Platform_Events <- read.csv("C:/Users/Owner/Downloads/Platform Events.csv")
+
+#Comprehensive_DF <- merge(Monthly_Assessments, Platform_Events, by = c('USERID', 'EVENTDATE'), all.x = TRUE)
+
+drop_list <- c("\\[", "\"", " ", "\\]")
+
+pattern <- paste0(drop_list, collapse = "|")
+
+Platform_Events$PLATFORMEVENTS <- gsub(pattern, "", Platform_Events$PLATFORMEVENTS)
+
+Platform_Events <- Platform_Events %>%
+  mutate(PLATFORMEVENTS = map_chr(str_split(PLATFORMEVENTS, ","), ~ paste(unique(str_trim(.x)), collapse = ",")))
+
+
+
+
+
+df_wider <- Platform_Events %>%
+  separate_longer_delim(PLATFORMEVENTS, delim = ",") %>%
+  mutate(PLATFORMEVENTS = trimws(PLATFORMEVENTS), present = 1) %>%
+  pivot_wider(
+    names_from = PLATFORMEVENTS,
+    values_from = present,
+    values_fill = 0,
+    values_fn = max # Ensures a 1 is kept if a word accidentally repeats in a cell
+  )
